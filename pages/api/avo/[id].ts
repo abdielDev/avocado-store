67267285d394a89b6ff282ae1d4dd094311f67cf
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-10-09 16:54:04 -0500

    The product detail view was build

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import DB from "@database";
 
-const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
+const AvoDetail = async (request: NextApiRequest, response: NextApiResponse) => {
   const db = new DB();
 
   const { id } = request.query;
@@ -11,4 +11,4 @@ const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
   response.status(200).json(entry);
 }
 
-export default allAvos;
+export default AvoDetail;
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const id = useRouter().query.id
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    window.fetch(`/api/avo/${id}`)
+    .then(response => response.json())
+    .then(response => {
+      setProduct(response)
+    })
+  }, [])
 
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto: {product.name}</h1>
     </section>
   )
 }
